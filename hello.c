7ee0d1c98886db3f7c81190ee68f5050f02459db
lamdatran <ctran11@huskers.unl.edu>
2020-08-19 19:46:21 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+ /**
+ * Author: Chau Tran
+ * Date: 2020/08/17
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char ***argv) {
+     
+   printf("Chau Tran!\n");
+   printf("Computer Science\n");
+ return 0;
+}
